1353d10c6c9fc204c56ba71602350f0efa391665
Mayur Tholiya <140947301+GN-Mayur@users.noreply.github.com>
2024-07-02 18:23:22 +0530

    Added demo SP

---
+++ VersionSQL/GN-MAYUR/SQLEXPRESS/AddressBook/dbo/Procedure/PR_Demo_StoredProcedure.sql
@@ -0,0 +1,5 @@
+﻿/****** Object:  Procedure [dbo].[PR_Demo_StoredProcedure]    Committed by VersionSQL https://www.versionsql.com ******/
+
+CREATE PROCEDURE [dbo].[PR_Demo_StoredProcedure]
+AS
+SELECT GETDATE()
